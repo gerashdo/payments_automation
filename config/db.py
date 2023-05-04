f3e5ee3e1f68ecc891de495051d9c548d76420f1
gerashdo <gerardo.jimenezarguelles@gmail.com>
2023-05-04 16:32:01 -0600

    Add .env.example for environment variables

--- config/db.py
+++ config/db.py
@@ -1,11 +1,11 @@
-
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "mysql+pymysql://root:Stoker.3@localhost:3306/payment_project"
+SQLALCHEMY_DATABASE_URL = os.getenv('DB_URL')
 
-engine = create_engine( SQLALCHEMY_DATABASE_URL )
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
 SessionLocal = sessionmaker(bind=engine)
 
